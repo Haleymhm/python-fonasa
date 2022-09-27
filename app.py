713556729b0365250c98926af50b3742f123c6d2
Haleym Huidalgo Moyetones <haleymhm@gmail.com>
2022-09-27 20:55:45 -0300

    :sparkles: Modulo Especialistas Medicos

--- app.py
+++ app.py
@@ -70,5 +70,71 @@ def update_contact(id):
         return redirect(url_for('specialty'))
 # FIN DE MODULO ESPECILIDADES
 
+# INICIO DE MODULO ESPECAILISTA
+@app.route('/specialist')
+def specialist():
+    specialists = mysql.connection.cursor()
+    specialists.execute('SELECT specialist.id, specialist.fullname, specialty.name  FROM specialist, specialty WHERE specialty.id = specialist.specialty_id')
+    data = specialists.fetchall()
+    specialists.close()
+    speciality = mysql.connection.cursor()
+    speciality.execute('SELECT * FROM specialty')
+    data1 = speciality.fetchall()
+    return render_template('specialist/index.html', specialists = data, specialties = data1)
+
+@app.route('/specialist-add', methods=['POST'])
+def add_specialist():
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        specialty_id = request.form['specialtyid']
+
+        specialist = mysql.connection.cursor()
+        sql = "INSERT INTO specialist (fullname, specialty_id) VALUES ('"+fullname+"','"+specialty_id+"')"
+        
+        specialist.execute(sql)
+        mysql.connection.commit()
+        specialist.close()
+        flash('EL Registro se ha registrado satisfactoriamente')
+        return redirect(url_for('specialist'))
+
+@app.route('/specialist-delete/<string:id>', methods=['POST', 'GET'])
+def specialist_delete(id):
+    specialist = mysql.connection.cursor()
+    specialist.execute('DELETE FROM specialist WHERE id = {0}'.format(id))
+    mysql.connection.commit()
+    flash('El registro se ha ELIMINADO satisfactoriamente')
+    return redirect(url_for('specialist'))
+
+@app.route('/specialist-edit/<id>', methods=['POST', 'GET'])
+def specialist_edit(id):
+    speciality = mysql.connection.cursor()
+    sql = "SELECT * FROM specialist WHERE id="+id
+    speciality.execute(sql)
+    data = speciality.fetchall()
+    speciality.close()
+
+    speciality = mysql.connection.cursor()
+    speciality.execute('SELECT * FROM specialty')
+    data1 = speciality.fetchall()
+    speciality.close()
+    
+    return render_template('specialist/edit.html', specialist = data[0], specialties = data1)
+
+@app.route('/specialist-update/<id>', methods=['POST', 'GET'])
+def specialist_update(id):
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        specialty_id = request.form['specialtyid']
+        speciality = mysql.connection.cursor()
+        sql ="UPDATE specialist SET fullname ='"+fullname+"', specialty_id='"+specialty_id+"' WHERE id="+id
+        speciality.execute(sql)
+        mysql.connection.commit()
+        speciality.close()
+        flash('EL Registro se ha actualizado satisfactoriamente')
+        return redirect(url_for('specialist'))
+# FIN DE MODULO ESPECIALISTA 
+
+
+
 if __name__ == '__main__':
     app.run(port = 3000, debug = True)
